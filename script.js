329d5263d70b17de968ef9a92290d09e797f45c8
Amir Fakouri <98204356+Amir-fa@users.noreply.github.com>
2023-02-23 20:21:48 +0330

    Weather App

---
+++ script.js
@@ -0,0 +1,36 @@
+let weather = {
+  apiKey: 'b6769f3d6b0b3b56faa408051f2bc00d',
+  fetchWeather(city) {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+
+  displayWeather(data) {
+    const { name } = data;
+    const { temp, humidity } = data.main;
+    const { icon, description } = data.weather[0];
+    const { speed } = data.wind;
+
+    document.querySelector('.city').innerText = `Weather in ${name}`;
+    document.querySelector('.temp').innerText = `${temp} °C`;
+    document.querySelector(
+      '.icon'
+    ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.humidity').innerText = `Humidity: ${humidity}%`;
+    document.querySelector('.wind').innerText = `Wind speed ${speed} km/h`;
+    document.body.style.backgroundImage = `url('https://source.unsplash.com/random/900x700/?${name}')`;
+  },
+  search() {
+    this.fetchWeather(document.querySelector('.search-bar').value);
+  },
+};
+
+document.querySelector('.search button').addEventListener('click', () => {
+  weather.search();
+});
+
+weather.fetchWeather('new york');
